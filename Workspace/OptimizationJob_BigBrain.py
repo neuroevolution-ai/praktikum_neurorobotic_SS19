526b9c35b6ead0f75ff5cee9f22e36799d1dda0e
Daniel <DanielZimmermann86@gmx.net>
2019-09-04 11:32:27 +0000

    Big braitenberg brain added

---
+++ Workspace/OptimizationJob_BigBrain.py
@@ -0,0 +1,305 @@
+import pyNN.nest as brain_sim
+import csv
+import numpy as np
+import optparse
+
+
+class brain_big(object):
+    
+    def __init__(self, WEIGHT_RED_TO_ACTOR, WEIGHT_RED_TO_GO_ON, WEIGHT_GREEN_BLUE_TO_ACTOR, WEIGHT_GO_ON_TO_RIGHT_ACTOR):  
+        sim = brain_sim
+
+        INPUT_PARAMS = {'a': 4.0,
+                        'b': 0.0000805,
+                        'delta_T': 2.0,
+                        'tau_w': 144.0,
+                        'v_spike': 0.0,
+                        'cm': .281, # ev. /1000
+                        'v_rest': -70.6,
+                        'tau_m': 9.3666667,
+                        'e_rev_E': 0.0,
+                        'e_rev_I': -80.0,
+                        'v_reset': -70.6,
+                        'v_thresh': -50.4,
+                        'tau_refrac': 10.0,
+                        'tau_syn_E': 5.,
+                        'tau_syn_I': 5.}
+
+        SENSORPARAMS = {'b': 0.0,
+                        'tau_w': 10.0,
+                        'v_spike': 0.0,
+                        'cm': 0.025,
+                        'v_rest': -60.5,
+                        'tau_m': 10.,
+                        'e_rev_E': 0.0,
+                        'e_rev_I': -75.0,
+                        'v_reset': -60.5,
+                        'v_thresh': -60.0,
+                        'tau_refrac': 10.0,
+                        'tau_syn_E': 2.5,
+                        'tau_syn_I': 7.5}
+
+        GO_ON_PARAMS = {'cm': .025,
+                        'v_rest': -60.5,
+                        'tau_m': 10.,
+                        'e_rev_E': 0.0,
+                        'e_rev_I': -75.0,
+                        'v_reset': -61.6,
+                        'v_thresh': -60.51,
+                        'tau_refrac': 10.0,
+                        'tau_syn_E': 2.5,
+                        'tau_syn_I': 7.5}
+
+        INTERMEDIATE_PARAMS = {'a': 4.0,
+                               'b': 0.0000805,
+                               'delta_T': 2.0,
+                               'tau_w': 144.0,
+                               'v_spike': 0.0,
+                               'cm': .281, # ev. /1000
+                               'v_rest': -70.6,
+                               'tau_m': 112.4,
+                               'e_rev_E': 0.0,
+                               'e_rev_I': -80.0,
+                               'v_reset': -70.6,
+                               'v_thresh': -50.4,
+                               'tau_refrac': 10.0,
+                               'tau_syn_E': 5.,
+                               'tau_syn_I': 5.}
+
+
+        self.population = sim.Population(709, sim.EIF_cond_alpha_isfa_ista())
+        self.population[:600].set(**INPUT_PARAMS)
+        self.population[600:605].set(**SENSORPARAMS)
+        self.population[605:606].set(**GO_ON_PARAMS)
+        self.population[606:608].set(**SENSORPARAMS)
+        self.population[608:708].set(**INTERMEDIATE_PARAMS)
+        self.population[708:709].set(**GO_ON_PARAMS)
+
+        SYNAPSE_PARAMS = {"delay": 0.1 }
+
+        # Synaptic weights
+        weight_red_to_actor = 1.5e-2  # was 1.5e-4
+        weight_red_to_go_on = 1000  # or -1.2e-3?
+        weight_green_blue_to_actor = 1.0
+        weight_go_on_to_right_actor = 1.4e-4  # was 1.4e-4
+        weight_eval_red = 2.5e-2
+        weight_eval_to_red_sensor = 8.75e-5  # was 1.0e-4, then 8.75e-5
+        weight_eval_to_bg_sensor = 5.0  # was 1.5, then 10.0, change it further
+        weight_red_to_blue_eval = 1000  # was 5e-8
+
+        synapse_red_to_actor = sim.StaticSynapse(weight=weight_red_to_actor, **SYNAPSE_PARAMS)
+        synapse_red_to_go_on = sim.StaticSynapse(weight=weight_red_to_go_on,**SYNAPSE_PARAMS)
+        synapse_green_blue_to_actor = sim.StaticSynapse(weight=weight_green_blue_to_actor,                                                        **SYNAPSE_PARAMS)
+        synapse_go_on_to_right_actor = sim.StaticSynapse(weight=weight_go_on_to_right_actor,
+                                                                **SYNAPSE_PARAMS)
+        synapse_eval_red = sim.StaticSynapse(weight=weight_eval_red, **SYNAPSE_PARAMS)
+        synapse_eval_to_red_sensor = sim.StaticSynapse(weight=weight_eval_to_red_sensor,
+                                                                **SYNAPSE_PARAMS)
+        synapse_eval_to_bg_sensor = sim.StaticSynapse(weight=weight_eval_to_bg_sensor,
+                                                                **SYNAPSE_PARAMS)
+        synapse_red_to_blue_eval = sim.StaticSynapse(weight=weight_red_to_blue_eval,
+                                                                **SYNAPSE_PARAMS)
+
+        # Connect neurons
+        circuit = self.population
+        
+        sim.Projection(circuit[602:603], circuit[607:608],
+                       connector=sim.AllToAllConnector(),
+                       synapse_type=synapse_red_to_actor,
+                       receptor_type='excitatory')
+        sim.Projection(circuit[603:604], circuit[606:607],
+                       connector=sim.AllToAllConnector(),
+                       synapse_type=synapse_red_to_actor,
+                       receptor_type='excitatory')
+
+
+        sim.Projection(circuit[600:602], circuit[605:606],
+                       connector=sim.AllToAllConnector(),
+                       synapse_type=synapse_red_to_go_on,
+                       receptor_type='inhibitory')
+
+
+        sim.Projection(circuit[604:605], circuit[607:608],
+                       connector=sim.AllToAllConnector(),
+                       synapse_type=synapse_green_blue_to_actor,
+                       receptor_type='excitatory')
+
+
+        sim.Projection(circuit[605:606], circuit[607:608],
+                       connector=sim.AllToAllConnector(),
+                       synapse_type=synapse_go_on_to_right_actor,
+                       receptor_type='excitatory')
+
+
+        # connect the left portion of the detector neurons to the left evaluator and the right portion to the right one
+        sim.Projection(circuit[0:300], circuit[608:658],
+                       connector=sim.FixedNumberPreConnector(6),
+                       synapse_type=synapse_eval_red,
+                       receptor_type='excitatory')
+
+        sim.Projection(circuit[300:600], circuit[658:708],
+                       connector=sim.FixedNumberPreConnector(6),
+                       synapse_type=synapse_eval_red,
+                       receptor_type='excitatory')
+
+
+        sim.Projection(circuit[608:658], circuit[600:601],
+                       connector=sim.AllToAllConnector(),
+                       synapse_type=synapse_eval_to_red_sensor,
+                       receptor_type='excitatory')
+        sim.Projection(circuit[608:658], circuit[602:603],
+                       connector=sim.AllToAllConnector(),
+                       synapse_type=synapse_eval_to_red_sensor,
+                       receptor_type='excitatory')
+        sim.Projection(circuit[658:708], circuit[601:602],
+                       connector=sim.AllToAllConnector(),
+                       synapse_type=synapse_eval_to_red_sensor,
+                       receptor_type='excitatory')
+        sim.Projection(circuit[658:708], circuit[603:604],
+                       connector=sim.AllToAllConnector(),
+                       synapse_type=synapse_eval_to_red_sensor,
+                       receptor_type='excitatory')
+
+
+        sim.Projection(circuit[600:602], circuit[604:605],
+                       connector=sim.AllToAllConnector(),
+                       synapse_type=synapse_red_to_blue_eval,
+                       receptor_type='inhibitory')
+
+        sim.Projection(circuit[708:709], circuit[604:605],
+                       connector=sim.OneToOneConnector(),
+                       synapse_type=synapse_eval_to_bg_sensor,
+                       receptor_type='excitatory')  # connect the "blue green evaluator" to the blue green sensor
+        
+        sim.initialize(circuit, v=circuit.get('v_reset'))
+        
+        self.sensor_red_left   = circuit[0:300]
+        self.sensor_red_right  = circuit[300:600]
+        self.sensor_green_blue = circuit[708]
+        self.actor_left        = circuit[606]
+        self.actor_right       = circuit[607]
+        
+    
+    def get_weights(self):
+        w1 = self._p1.get('weight', format='array')
+        w2 = self._p2.get('weight', format='array')
+        w3 = self._p3.get('weight', format='array')
+        w4 = self._p4.get('weight', format='array')
+        w5 = self._p5.get('weight', format='array')
+        w6 = self._p6.get('weight', format='array')
+        
+        return w1, w2, w3, w4, w5, w6
+    
+    
+class SpikeGenerator(object):
+    """the class handles the input for the brain"""
+    
+    def __init__(self, sensor_red_left, sensor_red_right, sensor_green_blue):
+        params_source=  {'start': 0.0, 'duration': float("inf"), 'rate': 0.0}
+        params_connec= {'weight':0.00015, 'receptor_type':'excitatory','delay':0.1}
+        
+        self.ssp_red_left_eye = brain_sim.create(brain_sim.SpikeSourcePoisson,  params_source)
+        self.ssp_red_right_eye = brain_sim.create(brain_sim.SpikeSourcePoisson,  params_source)
+        self.ssp_green_blue_eye = brain_sim.create(brain_sim.SpikeSourcePoisson,  params_source)
+        
+        brain_sim.connect(self.ssp_red_left_eye, sensor_red_left, **params_connec)
+        brain_sim.connect(self.ssp_red_right_eye, sensor_red_right, **params_connec)
+        brain_sim.connect(self.ssp_green_blue_eye, sensor_green_blue, **params_connec)
+        
+    def set_rates_from_ratios(self, red_left_rate, red_right_rate, non_red_rate):
+        try:
+            self.ssp_red_left_eye.set(rate=red_left_rate)
+            self.ssp_red_right_eye.set(rate=red_right_rate)
+            self.ssp_green_blue_eye.set(rate=non_red_rate)
+        except StopIteration:
+            pass
+       
+      
+class OutputGenerator(object):
+    def __init__(self, actor_left, actor_right):
+        LI_create = {'v_thresh': float('inf'),'cm': 1.0,'tau_m': 10.0,'tau_syn_E': 2.,
+            'tau_syn_I': 2.,'v_rest': 0.0,'v_reset': 0.0,'tau_refrac': 0.1,}
+        LI_connect = {'weight':0.00015, 'receptor_type':'excitatory','delay':0.1}
+        
+        self.left = brain_sim.create(brain_sim.IF_curr_alpha, LI_create)
+        self.right = brain_sim.create(brain_sim.IF_curr_alpha, LI_create)
+        
+        brain_sim.connect(actor_left, self.left, **LI_connect)
+        brain_sim.connect(actor_right, self.right,  **LI_connect)
+        
+        brain_sim.initialize(self.left, v=self.left.get('v_rest'))
+        brain_sim.initialize(self.right, v=self.right.get('v_rest'))
+        
+        self.left.record('v')
+        self.right.record('v')
+    
+    def get_current_voltage(self):
+        return self._get_current_voltage(self.left), self._get_current_voltage(self.right)
+    
+    def _get_current_voltage(self, cell):
+        return cell.get_data('v', clear=True).segments[0].filter(name="v")[0][-1,0].item()
+    
+    
+def step(*ratio):
+    
+    timestep_ms = 7
+    
+    # Set the rates from the image
+    spike_generator.set_rates_from_ratios(*ratio)
+    
+    # Run one step in the brain simulation
+    brain_sim.run(timestep_ms)
+        
+    # Get the current voltage of the output neurons
+    voltage_left, voltage_right = output_generator.get_current_voltage() #10ms    
+    
+    return voltage_left, voltage_right
+
+
+# Read weights from args
+parser = optparse.OptionParser()
+options, args = parser.parse_args()
+WEIGHT_RED_TO_ACTOR = float(args[0])
+WEIGHT_RED_TO_GO_ON = float(args[1])
+WEIGHT_GREEN_BLUE_TO_ACTOR = float(args[2])
+WEIGHT_GO_ON_TO_RIGHT_ACTOR = float(args[3])
+
+brain_sim.setup(timestep=0.1,min_delay=0.1,max_delay=4.0)
+b = brain_big(WEIGHT_RED_TO_ACTOR, WEIGHT_RED_TO_GO_ON, WEIGHT_GREEN_BLUE_TO_ACTOR, WEIGHT_GO_ON_TO_RIGHT_ACTOR)
+spike_generator = SpikeGenerator(b.sensor_red_left, b.sensor_red_right, b.sensor_green_blue)
+output_generator = OutputGenerator(b.actor_left, b.actor_right)
+
+output = []
+voltage_left_error = 0
+voltage_right_error = 0
+
+
+with open('output1.csv', 'r') as f:
+    reader = csv.reader(f)
+    logging_information_list = list(reader)
+
+    
+# Convert to numpy array 
+logging_information = np.asarray(logging_information_list, dtype=np.double)
+
+for row in logging_information:
+
+    # Get logging information from csv file
+    age, voltage_left1, voltage_right1, red_left_rate, red_right_rate, non_red_rate = row
+
+    # Simulate
+    voltage_left2, voltage_right2 = step(red_left_rate, red_right_rate, non_red_rate)
+
+    # Calculate errors
+    voltage_left_diff = abs(voltage_left2 - voltage_left1)
+    voltage_left_error += voltage_left_diff
+    voltage_right_diff = abs(voltage_right2 - voltage_right1)
+    voltage_right_error += voltage_right_diff
+
+    # Write into output array
+    output.append([voltage_left2, voltage_right2])
+
+error = voltage_left_error + voltage_right_error
+
+print([WEIGHT_RED_TO_ACTOR, WEIGHT_RED_TO_GO_ON, WEIGHT_GREEN_BLUE_TO_ACTOR, WEIGHT_GO_ON_TO_RIGHT_ACTOR])
+print("fitness = " + str(error) )
--- Workspace/OptimizationJob_SmallBrain.py
+++ Workspace/OptimizationJob_SmallBrain.py
@@ -3,11 +3,11 @@ import csv
 import numpy as np
 import optparse
 
-
-class brain(object):
+class brain_small(object):
     
     def __init__(self, WEIGHT_RED_TO_ACTOR, WEIGHT_RED_TO_GO_ON, WEIGHT_GREEN_BLUE_TO_ACTOR, WEIGHT_GO_ON_TO_RIGHT_ACTOR): 
         sim = brain_sim
+        
         SENSORPARAMS = {'v_rest': -60.5,
                         'cm': 0.025,
                         'tau_m': 10.,
@@ -43,12 +43,12 @@ class brain(object):
 
         SYN = sim.TsodyksMarkramSynapse(weight=abs(WEIGHT_RED_TO_ACTOR),
                                         delay=DELAY, **syn_params)
-        self._p1 = sim.Projection(presynaptic_population=CIRCUIT[2:3], 
+        sim.Projection(presynaptic_population=CIRCUIT[2:3], 
                        postsynaptic_population=CIRCUIT[7:8],
                        connector=sim.AllToAllConnector(),
                        synapse_type=SYN,
                        receptor_type='excitatory')
-        self._p2 = sim.Projection(presynaptic_population=CIRCUIT[3:4],
+        sim.Projection(presynaptic_population=CIRCUIT[3:4],
                        postsynaptic_population=CIRCUIT[6:7],
                        connector=sim.AllToAllConnector(),
                        synapse_type=SYN,
@@ -57,12 +57,12 @@ class brain(object):
 
         SYN = sim.TsodyksMarkramSynapse(weight=abs(WEIGHT_RED_TO_GO_ON),
                                         delay=DELAY, **syn_params)
-        self._p3 = sim.Projection(presynaptic_population=CIRCUIT[0:2],
+        sim.Projection(presynaptic_population=CIRCUIT[0:2],
                        postsynaptic_population=CIRCUIT[4:5],
                        connector=sim.AllToAllConnector(),
                        synapse_type=SYN,
                        receptor_type='inhibitory')
-        self._p4 = sim.Projection(presynaptic_population=CIRCUIT[0:2],
+        sim.Projection(presynaptic_population=CIRCUIT[0:2],
                        postsynaptic_population=CIRCUIT[5:6],
                        connector=sim.AllToAllConnector(),
                        synapse_type=SYN,
@@ -70,7 +70,7 @@ class brain(object):
 
         SYN = sim.TsodyksMarkramSynapse(weight=abs(WEIGHT_GREEN_BLUE_TO_ACTOR),
                                         delay=DELAY, **syn_params)
-        self._p5 = sim.Projection(presynaptic_population=CIRCUIT[4:5],
+        sim.Projection(presynaptic_population=CIRCUIT[4:5],
                        postsynaptic_population=CIRCUIT[7:8],
                        connector=sim.AllToAllConnector(),
                        synapse_type=SYN,
@@ -78,39 +78,36 @@ class brain(object):
 
         SYN = sim.TsodyksMarkramSynapse(weight=abs(WEIGHT_GO_ON_TO_RIGHT_ACTOR),
                                         delay=DELAY, **syn_params)
-        self._p6 = sim.Projection(presynaptic_population=CIRCUIT[5:6],
+        sim.Projection(presynaptic_population=CIRCUIT[5:6],
                        postsynaptic_population=CIRCUIT[7:8],
                        connector=sim.AllToAllConnector(),
                        synapse_type=SYN,
                        receptor_type='excitatory')
-
-    def get_pop(self):  
-        return self.population   
-    
-    def get_weights(self):
-        w1 = self._p1.get('weight', format='array')
-        w2 = self._p2.get('weight', format='array')
-        w3 = self._p3.get('weight', format='array')
-        w4 = self._p4.get('weight', format='array')
-        w5 = self._p5.get('weight', format='array')
-        w6 = self._p6.get('weight', format='array')
         
-        return w1, w2, w3, w4, w5, w6
-    
+        brain_sim.initialize(CIRCUIT, v=CIRCUIT.get('v_rest'))
+        
+        self.sensor_red_left   = CIRCUIT[slice(0, 3, 2)]  # CIRCUIT[0] and CIRCUIT[2]
+        self.sensor_red_right  = CIRCUIT[slice(1, 4, 2)]  # CIRCUIT[1] and CIRCUIT[3]
+        self.sensor_green_blue = CIRCUIT[4]
+        self.actor_left        = CIRCUIT[6]
+        self.actor_right       = CIRCUIT[7]
+   
     
     
 class SpikeGenerator(object):
     """the class handles the input for the brain"""
     
-    def __init__(self, population, interval=20.0, count_red_left=1/3, count_red_right=1/3, count_non_red=1/3):
+    def __init__(self, sensor_red_left, sensor_red_right, sensor_green_blue):
         params_source=  {'start': 0.0, 'duration': float("inf"), 'rate': 0.0}
         params_connec= {'weight':0.00015, 'receptor_type':'excitatory','delay':0.1}
+        
         self.ssp_red_left_eye = brain_sim.create(brain_sim.SpikeSourcePoisson,  params_source)
         self.ssp_red_right_eye = brain_sim.create(brain_sim.SpikeSourcePoisson,  params_source)
         self.ssp_green_blue_eye = brain_sim.create(brain_sim.SpikeSourcePoisson,  params_source)
-        brain_sim.connect(self.ssp_red_left_eye, population[slice(0, 3, 2)], **params_connec)
-        brain_sim.connect(self.ssp_red_right_eye, population[slice(1, 4, 2)], **params_connec)
-        brain_sim.connect(self.ssp_green_blue_eye, population[4], **params_connec)
+        
+        brain_sim.connect(self.ssp_red_left_eye, sensor_red_left, **params_connec)
+        brain_sim.connect(self.ssp_red_right_eye, sensor_red_right, **params_connec)
+        brain_sim.connect(self.ssp_green_blue_eye, sensor_green_blue, **params_connec)
         
     def set_rates_from_ratios(self, red_left_rate, red_right_rate, non_red_rate):
         try:
@@ -121,18 +118,21 @@ class SpikeGenerator(object):
             pass
         
         
-        
 class OutputGenerator(object):
-    def __init__(self, population):
+    def __init__(self, actor_left, actor_right):
         LI_create = {'v_thresh': float('inf'),'cm': 1.0,'tau_m': 10.0,'tau_syn_E': 2.,
             'tau_syn_I': 2.,'v_rest': 0.0,'v_reset': 0.0,'tau_refrac': 0.1,}
         LI_connect = {'weight':0.00015, 'receptor_type':'excitatory','delay':0.1}
+        
         self.left = brain_sim.create(brain_sim.IF_curr_alpha, LI_create)
         self.right = brain_sim.create(brain_sim.IF_curr_alpha, LI_create)
-        brain_sim.connect(population[6], self.left, **LI_connect)
-        brain_sim.connect(population[7], self.right,  **LI_connect)
+        
+        brain_sim.connect(actor_left, self.left, **LI_connect)
+        brain_sim.connect(actor_right, self.right,  **LI_connect)
+        
         brain_sim.initialize(self.left, v=self.left.get('v_rest'))
         brain_sim.initialize(self.right, v=self.right.get('v_rest'))
+        
         self.left.record('v')
         self.right.record('v')
     
@@ -168,11 +168,9 @@ WEIGHT_GREEN_BLUE_TO_ACTOR = float(args[2])
 WEIGHT_GO_ON_TO_RIGHT_ACTOR = float(args[3])
 
 brain_sim.setup(timestep=0.1,min_delay=0.1,max_delay=4.0)
-b = brain(WEIGHT_RED_TO_ACTOR, WEIGHT_RED_TO_GO_ON, WEIGHT_GREEN_BLUE_TO_ACTOR, WEIGHT_GO_ON_TO_RIGHT_ACTOR)
-population=b.get_pop()
-brain_sim.initialize(population, v=population.get('v_rest'))
-spike_generator = SpikeGenerator(population)
-output_generator = OutputGenerator(population)
+b = brain_small(WEIGHT_RED_TO_ACTOR, WEIGHT_RED_TO_GO_ON, WEIGHT_GREEN_BLUE_TO_ACTOR, WEIGHT_GO_ON_TO_RIGHT_ACTOR)
+spike_generator = SpikeGenerator(b.sensor_red_left, b.sensor_red_right, b.sensor_green_blue)
+output_generator = OutputGenerator(b.actor_left, b.actor_right)
 
 output = []
 voltage_left_error = 0
@@ -183,7 +181,6 @@ with open('output1.csv', 'r') as f:
     reader = csv.reader(f)
     logging_information_list = list(reader)
 
-    
     
 # Convert to numpy array 
 logging_information = np.asarray(logging_information_list, dtype=np.double)
